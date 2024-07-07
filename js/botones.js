74c5dc84cdefbe6abac4f17e59b53bd2f68f643a
jarolGabriel <jarolgabriel.jimenez70@gmail.com>
2024-07-07 17:57:26 -0500

    update nueva actualisacion de los botones

--- js/botones.js
+++ js/botones.js
@@ -1,6 +1,12 @@
 const importBtn = document.getElementById("importante");
 const lastestBtn = document.getElementById("el-ultimo");
 const topBtn = document.getElementById("top");
+const cardOcultar = document.getElementById("card-ocultar");
+
+const imageImportante = document.getElementById("image-importante");
+const imageElUltimo = document.getElementById("image-el-ultimo");
+
+//imageElUltimo.classList.add("hidden");
 
 const handleClick = (event) => {
   importBtn.classList.remove("hover-palabra-active");
@@ -8,6 +14,22 @@ const handleClick = (event) => {
   topBtn.classList.remove("hover-palabra-active");
 
   event.target.classList.add("hover-palabra-active");
+
+  switch (event.target.id) {
+    case "importante":
+      imageImportante.classList.remove("hidden");
+      imageElUltimo.classList.add("hidden");
+      cardOcultar.style.display = "block";
+      break;
+    case "el-ultimo":
+      imageImportante.classList.add("hidden");
+      imageElUltimo.classList.remove("hidden");
+      cardOcultar.style.display = "block";
+      break;
+    case "top":
+      cardOcultar.style.display = "none";
+      break;
+  }
 };
 
 importBtn.addEventListener("click", handleClick);
